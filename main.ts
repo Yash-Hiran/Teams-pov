26b01564aa9e73b7f9b9d0c389bbae001b8da846
Yash Hiran <yash.hiran@medly.com>
2021-09-24 09:58:07 +0530

    feat(slackResource): stuck with creating slack channel

--- main.ts
+++ main.ts
@@ -1,6 +1,6 @@
-import { Construct } from "constructs";
 import { App, S3Backend, TerraformStack, TerraformVariable } from "cdktf";
-import { SlackProvider } from './.gen/providers/slack';
+import { Construct } from "constructs";
+import { Conversation, SlackProvider } from './.gen/providers/slack';
 
 class MyStack extends TerraformStack {
   constructor(scope: Construct, name: string) {
@@ -24,5 +24,27 @@ const slackToken = new TerraformVariable(stack, "SLACK_TOKEN", {
 new SlackProvider(stack, "Slack", {
   token: slackToken.stringValue
 })
+
+ new Conversation(stack, "demo-conversation", {
+  actionOnDestroy: "none",
+  isPrivate: false,
+  name: "first-channel",
+})
+
+
+//  new Usergroup(stack, "slack-group", {
+//   handle: "demo-handle",
+//   name: "demo-grp"
+// })
+
+// new UsergroupChannels(stack, "slack-group-channels", {
+//   usergroupId: slackGroup.id,
+//   channels: [slackChannel.id]
+// })
+
+// new UsergroupMembers(stack, "slack-group-members", {
+//   usergroupId: "C02FHH7AU2F",
+//   members: ["Yash Hiran", "yash.hiran"]
+// })
 
 app.synth();
