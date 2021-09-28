1e138f8dcab42b4446252a804d345f94bb7bf2b3
Yash Hiran <yash.hiran@medly.com>
2021-09-28 13:02:51 +0530

    feat: create resource for multiple stacks using dependency stack

--- main.ts
+++ main.ts
@@ -2,49 +2,38 @@ import { App, S3Backend, TerraformStack, TerraformVariable } from "cdktf";
 import { Construct } from "constructs";
 import { Conversation, SlackProvider } from './.gen/providers/slack';
 
+interface MyStackConfig {
+  slackToken: TerraformVariable
+}
+
 class MyStack extends TerraformStack {
-  constructor(scope: Construct, name: string) {
+  constructor(scope: Construct, name: string, config: MyStackConfig) {
     super(scope, name);
 
     new S3Backend(this, {
       bucket: "access-team-local-run-test",
       key: "state/terraform.tfstate",
       region: "us-east-1"
-    });   
+    });
+
+    new SlackProvider(this, "Slack", {
+      token: config.slackToken.stringValue
+    })
+
+    new Conversation(this, "demo-conversation", {
+      actionOnDestroy: "none",
+      isPrivate: false,
+      name: "second-channel",
+    })
   }
 }
 
 const app = new App();
-const stack = new MyStack(app, "teams-poc");
 
-const slackToken = new TerraformVariable(stack, "SLACK_TOKEN", {
-  type: 'string'
-});
-
-new SlackProvider(stack, "Slack", {
-  token: slackToken.stringValue
+new MyStack(app, "teams-pov", {
+  slackToken: new TerraformVariable(, "SLACK_TOKEN", {
+    type: 'string'
+  })
 })
-
- new Conversation(stack, "demo-conversation", {
-  actionOnDestroy: "none",
-  isPrivate: false,
-  name: "first-channel",
-})
-
-
-//  new Usergroup(stack, "slack-group", {
-//   handle: "demo-handle",
-//   name: "demo-grp"
-// })
-
-// new UsergroupChannels(stack, "slack-group-channels", {
-//   usergroupId: slackGroup.id,
-//   channels: [slackChannel.id]
-// })
-
-// new UsergroupMembers(stack, "slack-group-members", {
-//   usergroupId: "C02FHH7AU2F",
-//   members: ["Yash Hiran", "yash.hiran"]
-// })
 
 app.synth();
